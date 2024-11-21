beae4baa2a4ec5b60be959f8a892c060a9483d7b
HCMYX <631714470@qq.com>
2024-11-21 17:18:57 +0800

    computed

---
+++ 备份/11_toRefs和toRef/App.vue
@@ -0,0 +1,23 @@
+// 写html结构
+<template>
+    <Preson/>
+</template>
+
+//JS 或 TS
+<script lang="ts">
+    import Preson from './components/Preson.vue';
+    export default {
+        name: 'App', //组件名称
+        components:{Preson}
+    }
+</script>
+
+// 样式
+<style>
+    .app{
+        background-color: #ebdcdc;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
---
+++ 备份/11_toRefs和toRef/components/Preson.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ person.name }}</h2>
+        <h2>姓名：{{ person.age }}</h2>
+        <button @click="changeName">修改姓名</button>
+        <button @click="changeAge"> 修改年龄</button>
+    </div>
+</template>
+
+
+<script  lang="ts" setup name = "Preson">
+    import {reactive,toRefs} from  'vue'
+    import {ref} from  'vue'
+
+    let person = reactive({
+        name : '张三',
+        age : 18
+    })
+
+    let {name,age} = toRefs(person)
+
+    function changeName(){
+        name.value += '~'
+    }
+
+    function changeAge(){
+        age.value += 1
+    }
+
+</script>
+<style>
+    .person{
+        background-color: #279dcb;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
