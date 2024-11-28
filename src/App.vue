eae067fd18a18001ccb1c6ba1bcc410030e7ebb9
HCMYX <631714470@qq.com>
2024-11-28 15:04:05 +0800

    路由node-moudle

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,70 @@
 // 写html结构
 <template>
-    <Preson/>
+    <div class="app">
+        <h2>Vue路由测试</h2>
+        <!--导航区-->
+        <div class="navigate">
+            <a href="#" class="active">首页</a>
+            <a href="#">新闻</a>
+            <a href="#">关于</a>
+        </div>
+        <!-- 展示区-->
+         <div class="main-content">
+            ????
+         </div>
+    </div>
 </template>
 
 //JS 或 TS
 <script lang="ts" setup name="App">
-    import Preson from './components/Preson.vue';
-
+    
 
 </script>
 
 // 样式
 <style>
-    .app{
-        background-color: #ebdcdc;
-        box-shadow: 0 0 10px;
+    .title{
+        text-align: center;
+        word-spacing: 5px;
+        margin: 30px 0;
+        height: 70px;
+        line-height: 70px;
+        background-image: linear-gradient(45deg, gray,white);
         border-radius: 10px;
-        padding: 20px;
+        box-shadow: 0 0 2px;
+        font-size: 30px;
+    }
+    .navigate{
+        display: flex;
+        justify-content: space-around;
+        margin: 0 100px;
+    }
+    .navigate a{
+        display: block;
+        text-align: center;
+        width: 90px;
+        height: 40px;
+        line-height: 40px;
+        border-radius: 10px;
+        background-color: gray;
+        text-decoration: none;
+        color: white;
+        font-size: 18px;
+        letter-spacing: 5px;
+    }
+    .navigate a.active{
+        background-color: #64967e;
+        color: #ffc268;
+        font-weight: 900;
+        text-shadow: 0 0 1px black;
+        font-family: 微软雅黑;
+    }
+    .main-content{
+        margin: 0 auto;
+        margin-top: 30px;
+        border-radius: 10px;
+        width: 90%;
+        height: 400px;
+        border: 1px solid;
     }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 // 引入createApp
 import {createApp} from 'vue'
 import App from './App.vue'
+//引入路由器
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
+
